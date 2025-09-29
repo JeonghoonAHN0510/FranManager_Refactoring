2a29ea1080e0f80f6c5d519483b2903ff3be1ad0
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-09-29 22:36:10 +0900

    feat : 어노테이션 구현 중 MySQLHandler : Select 자동매핑 구현

--- src/main/java/custom/annotation/MySQLHandler.java
+++ src/main/java/custom/annotation/MySQLHandler.java
@@ -2,10 +2,7 @@ package custom.annotation;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class MySQLHandler implements InvocationHandler {
     // DB 연결 정보 - 일단 여기에 정보 저장
@@ -43,16 +40,50 @@ public class MySQLHandler implements InvocationHandler {
                     } // for end
                 } // if end
                 ResultSet rs = ps.executeQuery();
+                // 5. 자동 매핑 구현
                 if (rs.next()){
-                    MemberDto dto = new MemberDto();
-                    dto.setName(rs.getString("name"));
-                    dto.setAge(rs.getInt("age"));
-                    return dto;
+                    // 5-1. 호출된 메소드의 반환 타입 가져오기
+                    Class<?> type = method.getReturnType();
+                    // 5-2. 반환 타입의 생성자를 사용하여 새로운 인스턴스를 생성
+                    Object obj = type.getDeclaredConstructor().newInstance();
+                    // 5-3. ResultSet의 컬럼 정보를 가져오기
+                    ResultSetMetaData metaData = rs.getMetaData();
+                    int columnCount = metaData.getColumnCount();
+                    // 5-4. 컬럼을 순회하며, 값 채우기
+                    for (int i = 1; i <= columnCount; i++){
+                        // 5-5. DB에서 컬럼명을 가져오기
+                        String columnName = metaData.getColumnName(i);
+                        // 5-6. 컬럼명에 해당하는 Setter 메소드명 만들기
+                        String setterName = toSetterName(columnName);
+                        // 5-7. Dto의 모든 메소드를 순회하면서 일치하는 Setter 찾기
+                        for (Method dtoMethod : type.getDeclaredMethods()){
+                            if (dtoMethod.getName().equals(setterName) && dtoMethod.getParameterCount() == 1){
+                                // 5-8. 찾았으면, ResultSet에서 값을 가져와서 동적으로 메소드 호출
+                                dtoMethod.invoke(obj, rs.getObject(columnName));
+                                break;
+                            } // if end
+                        } // for end
+                    } // for end
+                    return obj;
                 } // if end
             } catch (Exception e){
                 System.out.println(e.getMessage());
-            }
+            } // try-catch end
         } // if end
         return null;
+    } // func end
+
+    /**
+     * 컬럼명을 Setter 메소드명으로 변환하는 메소드
+     * @param columnName    데이터베이스 컬럼명
+     * @return String       변환된 Setter 메소드명
+     */
+    private String toSetterName(String columnName) {
+        StringBuilder setterName = new StringBuilder("set");
+        for (String part : columnName.split("_")){
+            setterName.append(part.substring(0, 1).toUpperCase());
+            setterName.append(part.substring(1));
+        } // for end
+        return setterName.toString();
     } // func end
 } // class end
